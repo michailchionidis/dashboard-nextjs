9725aa91b4394c46071f25fb6ae8701855f3f9c1
michailchionidis <michalis.kilkis@gmail.com>
2024-12-12 13:33:24 +0200

    Added vercel configurtions

--- prisma/prisma.ts
+++ prisma/prisma.ts
@@ -1,18 +1,3 @@
-import { PrismaClient } from '@prisma/client'
-import { getDatabaseUrl } from '@/app/lib/db'
-
-// Global caching for the Prisma client
-const globalForPrisma = global as unknown as {
-  prisma: PrismaClient | undefined
+export const getDatabaseUrl = () => {
+  return process.env.POSTGRES_URL || process.env.DATABASE_URL
 }
-
-export const prisma = globalForPrisma.prisma ?? new PrismaClient({
-  datasources: {
-    db: {
-      url: getDatabaseUrl()
-    }
-  },
-  log: ['query', 'error', 'warn']
-})
-
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
